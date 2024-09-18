608ac1637534d43fd3273884834d2b1769630d8e
http-sallein <sallein.caasi@gmail.com>
2024-09-18 14:14:43 -0300

    desafio processos seletivos

---
+++ DesafioContador/src/Main.java
@@ -0,0 +1,38 @@
+import Exceptions.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            throw exception;
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+        if(parametroDois > parametroUm) {
+
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for(int i = 1 ; i <= contagem ; i++)
+            System.out.println(i)
+        ;
+
+    }
+}
